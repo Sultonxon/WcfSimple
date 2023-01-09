3b6d8a7c496e7e3b93685892f964a8be4445a831
Sultonxon <91827090+Sultonxon@users.noreply.github.com>
2023-01-09 12:38:22 +0500

    Add project files.

---
+++ WcfSimple/ContactManager.svc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfSimple
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public class ContactManager1 : IContactManager1
+    {
+        private static List<Contact> contacts = new List<Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            WebOperationContext webOperationContext = WebOperationContext.Current;
+            OperationContext operationContext = OperationContext.Current;
+            var sessionId = operationContext.SessionId;
+            var host = operationContext.Host as ServiceHost;
+            
+            contacts.Add(contact);
+        }
+
+        public Contact[] GetContacts()
+        {
+            return contacts.ToArray();
+        }
+    }
+}
---
+++ WcfSimple/IContactManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfSimple
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IContactManager1" in both code and config file together.
+    [ServiceContract(SessionMode = SessionMode.Required)]
+    public interface IContactManager1
+    {
+        [OperationContract]
+        void AddContact(Contact contact);
+
+        [OperationContract]
+        Contact[] GetContacts();
+    }
+
+    [DataContract]
+    public class Contact
+    {
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+        
+        [OnSerializing]
+        public void OnSerializing(StreamingContext context)
+        {
+            
+        }
+    }
+
+    [DataContract]
+    [KnownType(typeof(Customer))]
+    public class Customer : Contact
+    {
+        [DataMember]
+        public int CustomerNumber { get; set; }
+    }
+}
